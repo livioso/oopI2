dbadadf5e8155b54a6f64cb6951fa049ab9fe9ae
Livio Bieri <livio@livio.li>
2014-03-15 18:14:19 +0100

    Example is self-explanatory

---
+++ 031014-UIs/src/introduction/mainframeExample01.java
@@ -0,0 +1,27 @@
+package introduction;
+
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.*;
+import java.awt.event.*;
+
+public class mainframeExample01 {
+	
+	public static void main(String[] args) {
+		JLabel msgLabel = new JLabel();
+		JButton noButton = new JButton(); 
+		
+		msgLabel.setText("Mwhahaahhaha");
+		noButton.setText("no");
+		
+		JFrame win = new JFrame("Message");
+		JPanel buttonbox = new JPanel();
+		     
+		win.getContentPane().setLayout(new BorderLayout()); buttonbox.setLayout(new FlowLayout()); buttonbox.add(noButton);
+		     
+		win.getContentPane().add(msgLabel, "Center");
+		win.getContentPane().add(buttonbox, "South");
+		
+		win.setVisible(true);
+	}
+}
---
+++ 031014-UIs/src/introduction/mainframeExample02.java
@@ -0,0 +1,25 @@
+package introduction;
+
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.*;
+
+public class mainframeExample02 extends JFrame{
+	
+	public static void main(String[] args) {
+		mainframeExample02 meinFenster = new mainframeExample02();
+		meinFenster.setVisible(true);
+	}
+	
+	public mainframeExample02() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+		
+		JButton myButton= new JButton("Hallo"); 
+		myButton.setForeground(Color.BLUE); 
+		myButton.setBackground(Color.GREEN); 
+		myButton.setFont(new Font("Serif", 1, 24)); 
+		
+		add(myButton);
+		pack();
+	}
+}
